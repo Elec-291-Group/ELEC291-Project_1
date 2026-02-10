f22f5e591dafb617a209c80b3ef75bd747ae684a
Rijwal Sangey <rijwalsangey@gmail.com>
2026-02-10 00:39:12 -0800

    cleanup

--- lm4040_test/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\Ayaan\Desktop\ELEC291-Project_1\lm4040_test\"
-"C:\Users\Ayaan\Desktop\291\CrossIDE\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Ayaan\Desktop\ELEC291-Project_1\lm4040_test\ADC_to_voltage_temp_7seg.asm"
-echo Crosside_Action Set_Hex_File C:\Users\Ayaan\Desktop\ELEC291-Project_1\lm4040_test\ADC_to_voltage_temp_7seg.HEX
